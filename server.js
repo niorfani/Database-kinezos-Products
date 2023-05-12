b2b060ca9f02f524fdcda4524f652f316b092cb9
Nikolaos Orfandis <niorfani123@gmail.com>
2023-05-12 17:12:30 +0300

    connect to mongo db

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 console.log('Hello');
 const express = require('express')//you can finde express to module file
 const app = express()
+const mongoose = require('mongoose')
 
 //route
 app.get('/', (req,res) => {
     res.send('Hello NODE API')
 
-}
-)
-app.listen(3000), ()=> {
+})
+app.get('/blog', (req,res) => {
+        res.send('Hello Blog My name is Nikos')
+})
+
+
+
+
+mongoose.
+connect('mongodb+srv://niorfani123:123456nikos@orfanidisapi.ubajhpr.mongodb.net/Node-Api?retryWrites=true&w=majority')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
     console.log('Node API app is running on port 3000')    
-}
+});
+    
+}).catch((error) => {
+    console.log(error)
+})
