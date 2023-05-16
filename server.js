20bfd949b70c265838e1763b4adc02543267b86e
Nikolaos Orfandis <niorfani123@gmail.com>
2023-05-16 14:21:24 +0300

    end of code

--- server.js
+++ server.js
@@ -48,21 +48,39 @@ app.post('/product', async(req, res) => {
     }
 })
 
-//update
-app.put('products/:id' , async(req, res) => {
+// update a product
+app.put('/products/:id', async(req, res) => {
     try {
         const {id} = req.params;
         const product = await Product.findByIdAndUpdate(id, req.body);
-        //we cannot find any data in database
+        // we cannot find any product in database
         if(!product){
-            return res.status(404).json({message: 'cannot find any product with ID ${id}'})
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
         }
-        res.status(200).json(product)
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+        
     } catch (error) {
         res.status(500).json({message: error.message})
     }
-} )
+})
 
+
+// delete a product
+
+app.delete('/products/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
 
 mongoose.
